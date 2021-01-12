57a4f10c15211b47699b264acdd81419ac332efd
LHJE <theeluke@gmail.com>
2021-01-12 16:12:56 -0700

    Update welcome/index_spec with test to click Log In & update via rubocop

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,21 +7,28 @@ RSpec.describe 'Welcome Page' do
       visit root_path
     end
 
-    it "can see welcome message" do
-      expect(page).to have_content("Welcome to the Image Repo!")
+    it 'can see welcome message' do
+      expect(page).to have_content('Welcome to the Image Repo!')
     end
 
-    it "can see login/register prompt" do
-      expect(page).to have_content("Please Log In or Register!")
+    it 'can see login/register prompt' do
+      expect(page).to have_content('Please Log In or Register!')
     end
 
-    it "can see login link" do
-      expect(page).to have_link("Log In")
+    it 'can see login link' do
+      expect(page).to have_link('Log In')
     end
 
-    it "can see register link" do
-      expect(page).to have_link("Register")
+    it 'can see register link' do
+      expect(page).to have_link('Register')
     end
 
+    it 'can send visitor to /login when Log In is clicked' do
+      within '.login' do
+        click_link 'Log In'
+      end
+
+      expect(current_path).to eq(login_path)
+    end
   end
 end
