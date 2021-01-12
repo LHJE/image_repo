7bb74ecd6beb2960e388477a8c0bbd9862d3cb55
LHJE <theeluke@gmail.com>
2021-01-12 16:34:13 -0700

    Update routes with logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
+  
   get '/registration', to: 'users#new', as: :registration
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#logout'
+
   resources :users, only: [:create]
 end
