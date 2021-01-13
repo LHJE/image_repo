dfe817a9b70224e253206c9c74161a0130bbd1de
LHJE <theeluke@gmail.com>
2021-01-12 20:47:37 -0700

    Update image index spec to ensure only users can upload images

--- spec/features/images/index_spec.rb
+++ spec/features/images/index_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 include ActionView::Helpers::NumberHelper
 
-RSpec.describe 'Image Page' do
+RSpec.describe 'Images Index' do
   describe 'As a visitor' do
     before :each do
+      visit '/images'
+    end
+
+    it 'can see base headers' do
+      expect(page).to have_content("The Image Repo!")
+      expect(page).to have_content("The Images, most recent at the top:")
+      expect(page).to_not have_link("Upload An Image")
+      expect(page).to have_content("Search:")
+    end
+  end
+
+  describe 'As a user' do
+    before :each do
+      @user = User.create!(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit '/images'
     end
 
