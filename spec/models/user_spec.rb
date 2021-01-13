93736d6657af625d4cb986a6627b175c553e599c
LHJE <theeluke@gmail.com>
2021-01-12 20:24:43 -0700

    Update user spec to have many `images`

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  describe 'Relationships' do
+    it {should have_many :images}
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
