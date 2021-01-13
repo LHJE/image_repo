8a8b45d375c574076a9fb28b675bad22698d4382
LHJE <theeluke@gmail.com>
2021-01-12 17:07:58 -0700

    Update nav spec to test for logged in user

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -35,4 +35,23 @@ RSpec.describe 'Site Navigation' do
     end
   end
 
+  describe 'As a User' do
+    before :each do
+      @user = User.create(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    describe 'I see a nav bar where I can link to' do
+      it 'the welcome page' do
+        visit '/images'
+
+        within 'nav' do
+          click_link 'Home'
+        end
+
+        expect(current_path).to eq(root_path)
+      end
+
+    end
+  end
 end
