b8071b30a9e6706c14cc5c43175c327a43e8cc39
LHJE <theeluke@gmail.com>
2021-01-12 17:15:19 -0700

    Update sessions controller with log out method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
   def new
     return unless current_user
+
     flash[:notice] = 'You are already logged in!'
     login_redirect(current_user)
+  end
+
+  def logout
+    session.delete(:user_id)
+    flash[:notice] = 'You have been logged out!'
+    redirect_to root_path
   end
 
   private
