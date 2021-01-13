18ee2048937925d5b876f5845fa894316742644d
LHJE <theeluke@gmail.com>
2021-01-12 19:16:38 -0700

    Add images index spec

---
+++ spec/features/images/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+include ActionView::Helpers::NumberHelper
+
+RSpec.describe 'Image Page' do
+  describe 'As a visitor' do
+    before :each do
+      visit '/images'
+    end
+
+    it 'can see base headers' do
+      expect(page).to have_content("The Image Repo!")
+      expect(page).to have_content("The Images, most recent at the top:")
+      expect(page).to have_content("Upload:")
+      expect(page).to have_content("Search:")
+    end
+  end
+end
