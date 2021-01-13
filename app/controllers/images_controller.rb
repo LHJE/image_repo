55e32f890cfd4edac3e1d28c3ca566abe53e17f4
LHJE <theeluke@gmail.com>
2021-01-12 20:53:35 -0700

    Update images controller to have new method

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,12 @@
 class ImagesController < ApplicationController
   def index; end
+
+  def new
+    if !current_user
+      flash[:notice] = 'This Page Only Accessible by Authenticated Users. Please Log In.'
+      redirect_to '/images'
+    else
+      @image = Image.new
+    end
+  end
 end
