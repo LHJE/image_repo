558a4800866ea5686979c69d413fd70f7ad62d5a
LHJE <theeluke@gmail.com>
2021-01-14 16:37:56 -0700

    Update images controller so show has @image and not @images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,7 +10,7 @@ class ImagesController < ApplicationController
   # GET /images/1
   # GET /images/1.json
   def show
-    @images = Image.where(id: params[:id])[0]
+    @image = Image.where(id: params[:id])[0]
   end
 
   # GET /images/new
