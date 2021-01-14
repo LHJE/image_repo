1d937bfdaea87219ad2676262cac90034084f4b1
LHJE <theeluke@gmail.com>
2021-01-14 16:34:39 -0700

    Update image new spec to match new language

--- spec/features/images/new_spec.rb
+++ spec/features/images/new_spec.rb
@@ -6,27 +6,27 @@ RSpec.describe 'New Image', type: :view do
     @user_1 = User.create(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
     @user_2 = User.create(name: 'Jackie Chan', email: 'its@jackie.com', password: 'securepassword', password_confirmation: 'securepassword')
     @image_1 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString1",
+      body: "MyText1",
+      keyword: "MyString1",
       user_id: @user_1.id
     ))
     @image_2 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString2",
+      body: "MyText2",
+      keyword: "MyString2",
       user_id: @user_1.id
     ))
     @image_3 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString3",
+      body: "MyText3",
+      keyword: "MyString3",
       user_id: @user_2.id
     ))
     @image_4 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString4",
+      body: "MyText4",
+      keyword: "MyString4",
       user_id: @user_2.id
     ))
 
@@ -67,33 +67,28 @@ RSpec.describe 'New Image', type: :view do
       expect(Image.all[-1].keyword).to eq(@new_image.keyword)
     end
 
-    it 'cannot upload an image without keyword' do
+    it 'cannot upload an image without all fields filled in' do
       visit '/images/new'
-
 
       click_button 'Create Image'
 
-      expect(page).to have_content("keyword: [\"can't be blank\"]")
-    end
-
-    it 'cannot upload an image without Url' do
-      visit '/images/new'
-
-      fill_in 'Keyword', with: @new_image[:keyword]
-      click_button 'Create Image'
-
-      expect(page).to have_content("url: [\"can't be blank\"]")
+      expect(page).to have_content("3 errors prohibited this image from being saved:")
+      expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content("Body can't be blank")
+      expect(page).to have_content("Keyword can't be blank")
     end
 
     it 'can see an uploaded an image on the index, at the top' do
       visit '/images/new'
 
+      fill_in 'Title', with: @new_image[:title]
+      fill_in 'Body', with: @new_image[:body]
       fill_in 'Keyword', with: @new_image[:keyword]
 
       click_button 'Create Image'
 
       within "#image-#{Image.all[-1].id}" do
-        expect(page).to have_content(@new_image.keyword.capitalize)
+        expect(page).to have_content(@new_image.title)
       end
     end
   end
