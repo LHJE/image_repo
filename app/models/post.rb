e93803d1c1972ab3aee5681383a18262c5355fe8
LHJE <theeluke@gmail.com>
2021-01-13 22:28:00 -0700

    Update post model so it belongs to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
   validates :title, presence: true
   validates :body, presence: true
   validates :keyword, presence: true
-  # validate :image_type
-
-  private
-
-  # def image_type
-  #   if image.attached? == false
-  #     errors.add(:image, 'art missing!')
-  #   end
-  #   if !image.content_type.in?(%('image/jpeg image/png'))
-  #     errors.add(:image, 'needs to be a JPEG or PNG')
-  #   end
-  # end
 end
