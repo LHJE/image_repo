7199beb42878aa4c58d0d1caec0b2ec4429b76b8
LHJE <theeluke@gmail.com>
2021-01-14 16:34:34 -0700

    Update image show spec to match new language

--- spec/features/images/show_spec.rb
+++ spec/features/images/show_spec.rb
@@ -6,27 +6,27 @@ RSpec.describe 'Images Show', type: :view do
     @user_1 = User.create(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
     @user_2 = User.create(name: 'Jackie Chan', email: 'its@jackie.com', password: 'securepassword', password_confirmation: 'securepassword')
     @image_1 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString1",
+      body: "MyText1",
+      keyword: "MyString1",
       user_id: @user_1.id
     ))
     @image_2 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString2",
+      body: "MyText2",
+      keyword: "MyString2",
       user_id: @user_1.id
     ))
     @image_3 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString3",
+      body: "MyText3",
+      keyword: "MyString3",
       user_id: @user_2.id
     ))
     @image_4 = assign(:image, Image.create!(
-      title: "MyString",
-      body: "MyText",
-      keyword: "MyString",
+      title: "MyString4",
+      body: "MyText4",
+      keyword: "MyString4",
       user_id: @user_2.id
     ))
   end
@@ -37,9 +37,11 @@ RSpec.describe 'Images Show', type: :view do
     end
 
     it 'click on image and be sent to that image show page' do
+
       within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
+        click_link 'Show'
       end
+
 
       expect(current_path).to eq("/images/#{@image_1.id}")
     end
@@ -47,10 +49,10 @@ RSpec.describe 'Images Show', type: :view do
     it "can see all information about the image, but not update or delete" do
 
       within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
+        click_link 'Show'
       end
 
-      expect(page).to have_content(@image_1.keyword.capitalize)
+      expect(page).to have_content(@image_1.title)
       expect(page).to_not have_link("Update Image")
       expect(page).to_not have_link("Delete Image")
     end
@@ -65,7 +67,7 @@ RSpec.describe 'Images Show', type: :view do
 
     it 'click on image and be sent to that image show page' do
       within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
+        click_link 'Show'
       end
 
       expect(current_path).to eq("/images/#{@image_1.id}")
@@ -73,31 +75,22 @@ RSpec.describe 'Images Show', type: :view do
 
     it "can see all information about the image, including update and delete" do
       within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
+        click_link 'Show'
       end
 
-      expect(page).to have_content(@image_1.keyword.capitalize)
-      expect(page).to have_link("Update Image")
-      expect(page).to have_link("Delete Image")
+      expect(page).to have_content(@image_1.title)
+      expect(page).to have_link("Edit")
+      expect(page).to have_link("Back")
     end
 
     it "can click on Update Image and be sent to edit page" do
       within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
+        click_link 'Show'
       end
 
-      click_link("Update Image")
+      click_link("Edit")
       expect(current_path).to eq("/images/#{@image_1.id}/edit")
     end
 
-    it "can click on Update Image and be sent to edit page" do
-      within "#image-#{@image_1.id}" do
-        click_link @image_1.keyword.capitalize
-      end
-
-      click_link("Delete Image")
-      expect(current_path).to eq("/images")
-      expect(Image.where(id: @image_1.id)).to eq([])
-    end
   end
-  end
+end
