6d69617a63622cffbb153ecd33cb11860d97b15f
LHJE <theeluke@gmail.com>
2021-01-14 12:46:44 -0700

    Update image index spec with new object creation method, routing, logic

--- spec/features/images/index_spec.rb
+++ spec/features/images/index_spec.rb
@@ -1,19 +1,41 @@
 require 'rails_helper'
 include ActionView::Helpers::NumberHelper
 
-RSpec.describe 'Images Index' do
+RSpec.describe 'Images Index', type: :view do
   describe 'As any user' do
     before :each do
-      @user = User.create(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
-      @image_1 = @user.images.create(keyword: "cat", url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=518&q=80")
-      @image_2 = @user.images.create(keyword: "dog", url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20122208/Samoyed-standing-in-the-forest.jpg")
-      @image_3 = @user.images.create(keyword: "mouse", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/%D0%9C%D1%8B%D1%88%D1%8C_2.jpg/440px-%D0%9C%D1%8B%D1%88%D1%8C_2.jpg")
-      @image_4 = @user.images.create(keyword: "moose", url: "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_3481,w_3481,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/moose.shutter_t5itks.jpg")
-      visit '/images'
+      @user_1 = User.create(name: 'Morgan', email: 'morgan@example.com', password: 'securepassword', password_confirmation: 'securepassword')
+      @user_2 = User.create(name: 'Jackie Chan', email: 'its@jackie.com', password: 'securepassword', password_confirmation: 'securepassword')
+      @image_1 = assign(:image, Image.create!(
+        title: "MyString",
+        body: "MyText",
+        keyword: "MyString",
+        user_id: @user_1.id
+      ))
+      @image_2 = assign(:image, Image.create!(
+        title: "MyString",
+        body: "MyText",
+        keyword: "MyString",
+        user_id: @user_1.id
+      ))
+      @image_3 = assign(:image, Image.create!(
+        title: "MyString",
+        body: "MyText",
+        keyword: "MyString",
+        user_id: @user_2.id
+      ))
+      @image_4 = assign(:image, Image.create!(
+        title: "MyString",
+        body: "MyText",
+        keyword: "MyString",
+        user_id: @user_2.id
+      ))
     end
 
-    describe 'as a visitor' do
+    describe 'as a visitor', type: :feature do
       it 'can see base headers' do
+        visit '/images'
+
         expect(page).to have_content("The Image Repo!")
         expect(page).to have_content("The Images, most recent at the top:")
         expect(page).to_not have_link("Upload An Image")
@@ -25,6 +47,8 @@ RSpec.describe 'Images Index' do
       end
 
       it "can see images that have been uploaded" do
+        visit '/images'
+
         within "#image-#{@image_1.id}" do
           expect(page).to have_content(@image_1.keyword.capitalize)
         end
@@ -40,9 +64,9 @@ RSpec.describe 'Images Index' do
       end
     end
 
-    describe 'As a user' do
+    describe 'As a user', type: :feature do
       before :each do
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
         visit '/images'
       end
 
